671bdabfdb9bf0052235493ab4a8ae0301127bce
ngoctruc16 <ngoctruc1697@gmail.com>
2024-09-12 23:18:01 +0700

    Standardize JSON responses

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
+const JSend = require('./jsend');
 const contactsRouter = require('./routes/contacts.router');
 
 const app = express();
@@ -10,9 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    return res.json({
-        message: 'ok'
-    });
+    return res.json(JSend.success());
 });
 
 contactsRouter.setup(app);
